afcb7522feb279c43d82efb60e8b4b942a9798ef
shankariya, saurabh <sshankariya@gmail.com>
2016-04-07 23:02:39 +0530

    updates

--- client/js/controllers/generate.controller.js
+++ client/js/controllers/generate.controller.js
@@ -5,7 +5,15 @@ angular.module('wsdlApp')
     .controller('generateCtrl', ['$scope', '$log', 'wsdlAPI', 'appConstants', function($scope, $log, wsdlAPI, appConstants) {
        var me = this;
        me.hasError = false;
-       me.wsdlRequestObj = {};
+       me.wsdlObject = {};
+       
+       //watch function for state value to display/hide city text box
+        $scope.$watch('me.wsdlObject.serviceName', function(newValue, oldValue, scope) {
+               if(typeof newValue !== 'undefined' && newValue !== "" && newValue !== oldValue)
+                    me.wsdlObject.targetNamespace = 'http://service.lfg.com/' + me.wsdlObject.serviceName;
+                else
+                    me.wsdlObject.targetNamespace = 'http://service.lfg.com/';
+        });
        
        $scope.generateWsdl = function () {
            wsdlAPI.generateWSDL(me.wsdlRequestObj).then( function(data) {
